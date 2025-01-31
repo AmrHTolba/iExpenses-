5bf00d558d8f58aa7ab891e23be28a7295eef87b
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-01 01:42:23 +0200

    Created a button to create rows for a list
    Adding Edit button to the toolbar and func to remove selected row

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,19 +6,76 @@
 //
 
 import SwiftUI
+import Observation
+
+// MARK: - Class
+
+@Observable
+class User {
+    var firstName = ""
+    var lastName = ""
+}
+
+
+// MARK: - Structs
 
 struct ContentView: View {
+    
+    // MARK: - Variables
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    @State private var user = User()
+    @State private var showingSheet = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
         }
-        .padding()
+//        Button("Show New View") {
+//            showingSheet.toggle()
+//        }
+//        .sheet(isPresented: $showingSheet) {
+//            SecondView(name: "Hah noob")
+//        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
+
+
+//struct SecondView: View {
+//    
+//    let name: String
+//    @Environment(\.dismiss) var dismiss
+//    
+//    var body: some View {
+//        Text("You wrote: \(name)")
+//        Button("Dismiss") {
+//            dismiss()
+//        }
+//    }
+//}
 
 #Preview {
     ContentView()
 }
+
+
+
